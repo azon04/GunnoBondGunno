2d0bdd3b0afab59677f262848a03faea4dfb53a6
yusufardi97 <yusufardi97@gmail.com>
2013-04-30 21:55:27 +0700

    player.cs bisa geser player sama ubah angle

---
+++ GunnoBond/GunnoBond/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GunnoBond
+{
+    class Player
+    {
+        private string peerID;
+        private int healthPoint;
+        private Vector2 position;
+
+    }
+}
