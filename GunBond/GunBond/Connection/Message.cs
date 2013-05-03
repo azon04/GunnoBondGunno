4cd7f468b2da2d73889b39293853dc9b92ff8221
Nurul Fithria Lubis <nfithrialubis@gmail.com>
2013-05-03 11:27:30 +0700

    Parse message, resize bullet

--- GunBond/GunBond/Connection/Message.cs
+++ GunBond/GunBond/Connection/Message.cs
@@ -12,6 +12,8 @@ namespace GunBond.Connection
         public byte msgCode; //1 byte tipe message
         string tag = "GunBond";
         
+
+
         //buat posisi player
         public Vector2 playerPos; //posisi player
         public float playerRot; //angle tembak player
@@ -32,10 +34,15 @@ namespace GunBond.Connection
         //buat fire skenario3
         public Vector2 bulletPos;
 
+        //buat init
+        public int playerTexture;
+        public Vector2 playerPos0;
+
         public static byte POS = 0;
         public static byte KEEP_ALIVE = 1;
         public static byte FIRE = 2;
         public static byte NEXT_PLAYER = 3;
+        public static byte INIT = 4;
         
         public Message() //ctor
         {
@@ -45,18 +52,39 @@ namespace GunBond.Connection
 
         public void Parse(byte[] iMsg) {
             if (validMsg(iMsg)) {
+                //format msg = <tag><peerid><kodemsg><data>
+                PeerID = Encoding.ASCII.GetString(SubBytes(iMsg, 7, 4));
                 msgCode = iMsg[11];
                 Console.WriteLine(msgCode.ToString());
 
                 if (msgCode == 0) {
                     //process POS msg
+                    String msgData = Encoding.ASCII.GetString(SubBytes(iMsg, 12, iMsg.Length-12));
+                    String[] info = msgData.Split('|');
+                    String[] VectorPos = info[0].Split(',');
+                    playerPos.X = float.Parse(VectorPos[0]);
+                    playerPos.Y = float.Parse(VectorPos[1]);
+                    playerRot = float.Parse(info[1]);
+                    playerOrt = int.Parse(info[2]);
 
                 } else if (msgCode == 1) {
                     //process KEEP_ALIVE msg
+                    HP = iMsg[12];
                 } else if (msgCode == 2) {
                     //process FIRE msg
+                    String msgData = Encoding.ASCII.GetString(SubBytes(iMsg, 12, iMsg.Length - 12));
+                    elapsedTime = float.Parse(msgData);
                 } else if (msgCode == 3) {
                     //process NEXT_PLAYER msg
+                    nextPlayer = Encoding.ASCII.GetString(SubBytes(iMsg, 12, 4));
+                } else if (msgCode == 4) {
+                    //process INIT msg
+                    playerTexture = iMsg[12];
+                    String msgData = Encoding.ASCII.GetString(SubBytes(iMsg, 13, iMsg.Length - 13));
+                    String[] VectorPos0 = msgData.Split(',');
+                    playerPos0.X = float.Parse(VectorPos0[0]);
+                    playerPos0.Y = float.Parse(VectorPos0[1]);
+
                 } else {
                     Console.WriteLine("Message Tidak Valid");
                 }
@@ -97,35 +125,48 @@ namespace GunBond.Connection
             tempList.AddRange(Encoding.ASCII.GetBytes(PeerID));
             tempList.Add(msgCode);
 
-            //if (msgCode == 0) {
-            //    tempList.Add(POS);
-            //    tempList.AddRange(Encoding.ASCII.GetBytes("" + playerPos.X + "," + playerPos.Y + "|"));
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(playerRot.ToString() + "|"));
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(playerOrt));
-            //} else if (msgCode == 1) {
-            //    tempList.Add(KEEP_ALIVE);
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(HP));
-            //} else if (msgCode == 2) {
-            //    tempList.Add(FIRE);
+            if (msgCode == 0)
+            {
+                tempList.Add(POS);
+                tempList.AddRange(Encoding.ASCII.GetBytes("" + playerPos.X + "," + playerPos.Y + "|"));
+                tempList.AddRange(Encoding.ASCII.GetBytes(playerRot.ToString() + "|"));
+                tempList.AddRange(Encoding.ASCII.GetBytes(playerOrt.ToString()));
+            }
+            else if (msgCode == 1)
+            {
+                tempList.Add(KEEP_ALIVE);
+                tempList.AddRange(BitConverter.GetBytes(HP));
+                //tempList.AddRange(Encoding.ASCII.GetBytes(HP.ToString()));
+            }
+            else if (msgCode == 2)
+            {
+                tempList.Add(FIRE);
 
-            //    /*
-            //    //skenario1
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(bulletV0)); //ambil jadi string
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(playerRot));
-            //     */
+                /*
+                //skenario1
+                tempList.AddRange(Encoding.ASCII.GetBytes(bulletV0)); //ambil jadi string
+                tempList.AddRange(Encoding.ASCII.GetBytes(playerRot));
+                 */
 
-                
-            //    //skenario2
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(elapsedTime));
-                
-            //    /*
-            //    //skenario3
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(bulletPos)); //
-            //     */
-            //} else if(msgCode == 3) {
-            //    tempList.Add(Encoding.ASCII.GetBytes(NEXT_PLAYER));
-            //    tempList.AddRange(Encoding.ASCII.GetBytes(nextPlayer));
-            //}
+
+                //skenario2
+                tempList.AddRange(Encoding.ASCII.GetBytes(elapsedTime.ToString("0.000")));
+
+                /*
+                //skenario3
+                tempList.AddRange(Encoding.ASCII.GetBytes(bulletPos)); //
+                 */
+            }
+            else if (msgCode == 3)
+            {
+                tempList.Add(NEXT_PLAYER);
+                tempList.AddRange(Encoding.ASCII.GetBytes(nextPlayer));
+            }
+            else if (msgCode == 4)
+            {
+                tempList.AddRange(BitConverter.GetBytes(playerTexture));
+                tempList.AddRange(Encoding.ASCII.GetBytes("" + playerPos0.X + "," + playerPos0.Y));
+            }
 
             return tempList.ToArray();
         }
